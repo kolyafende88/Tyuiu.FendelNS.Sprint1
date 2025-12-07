3b8e9ce699f52860377374bd8fc5edd098119979
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-07 22:49:19 +0500

    Task3

---
+++ Tyuiu.FendelNS.Sprint1.Task3.V4.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.FendelNS.Sprint1.Task3.V4.Lib
+{
+    public class DataService : ISprint1Task3V4
+    {
+        public double PurchaseAmount(double priceNotebook, double priceCover, int amount)
+        {
+            return (priceNotebook * amount) + (priceCover * amount);
+        }
+    }
+}
