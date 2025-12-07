858e6abf474f8f5ba42a5a9cee76f57e8f073b6c
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-07 21:47:46 +0500

    Add project files.

---
+++ Tyuiu.FendelNS.Sprint1.Task0.V25.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.FendelNS.Sprint1.Task0.V25.Lib
+{
+    public class DataService : ISprint1Task0V25
+    {
+        public double Calculate()
+        {
+            return (30 / 6 - 4) * 3;
+        }
+    }
+}
---
+++ Tyuiu.FendelNS.Sprint1.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.FendelNS.Sprint1.Task0.V25.Lib;
+namespace Tyuiu.FendelNS.Sprint1.Task0.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(3, res);
+        }
+    }
+}
