4efbac8352b8a486f1f2c9a83d311262d3b2854d
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-07 23:38:59 +0500

    Task6

---
+++ Tyuiu.FendelNS.Sprint1.Task6.V16.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.FendelNS.Sprint1.Task6.V16.Lib;
+namespace Tyuiu.FendelNS.Sprint1.Task6.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpressions()
+        {
+            DataService ds = new DataService();
+            string text = "Hello World!?";
+            bool res = ds.CheckSpecSymbols(text);
+            bool wait = true;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.FendelNS.Sprint1.Task6.V16/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.FendelNS.Sprint1.Task6.V16.Lib;
+namespace Tyuiu.FendelNS.Sprint1.Task6.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Фенедль Н. С.| СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Cпринт #1                                                               *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Фендель Николай Сергеевич | СМАРТб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст. Проверить, что в строке  *");
+            Console.WriteLine("* есть восклицание (!) и вопрос (?).                                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите текст для проверки наличия ! и ?:");
+            string textcheck = Console.ReadLine();
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+            DataService ds = new DataService();
+            Console.WriteLine(ds.CheckSpecSymbols(textcheck));
+            Console.ReadKey();
+        }
+    }
+}
