828c53b97867a159cf3c1562e44e977276ad6d27
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-07 22:59:39 +0500

    Task4

---
+++ Tyuiu.FendelNS.Sprint1.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.FendelNS.Sprint1.Task4.V1.Lib;
+namespace Tyuiu.FendelNS.Sprint1.Task4.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            Assert.AreEqual((Math.Round(1 / Math.Sqrt((x + 2)), 3)), ds.Calculate(x));
+
+        }
+    }
+}
