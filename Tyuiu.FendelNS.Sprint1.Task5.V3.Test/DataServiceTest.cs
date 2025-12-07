f4d32246ae3fe71043f21b08cb5833317fc9c5e9
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-07 23:28:40 +0500

    Task5

---
+++ Tyuiu.FendelNS.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.FendelNS.Sprint1.Task5.V3.Lib;
+namespace Tyuiu.FendelNS.Sprint1.Task5.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            int x = 54321;
+            DataService ds = new DataService();
+            int res = ds.Calculate(x);
+
+            int wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.FendelNS.Sprint1.Task5.V3/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.FendelNS.Sprint1.Task5.V3.Lib;
+namespace Tyuiu.FendelNS.Sprint1.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Фендель Н. С. | СМАРТ6-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Фендель Николай Сергеевич  | СМАРТб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Присвоить целой переменной h третью от конца цифру в записи             *");
+            Console.WriteLine("* положительного целого числа k (например, если k=130985, то h=9).        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int k;
+
+            Console.WriteLine("Введите значение k:");
+            k = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Третья от конца цифра в записи положительного целого числа k равна: " + ds.Calculate(k));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
